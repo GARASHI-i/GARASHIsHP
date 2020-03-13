1e6c6c3681b857c2513ddc056cf41998556a8606
GARASHI-i <62157794+GARASHI-i@users.noreply.github.com>
2020-03-14 08:09:59 +0900

    Delete diary.js

--- diary.js
+++
@@ -1,9 +0,0 @@
-$(function()
-        {
-          $(`#day-title`).hide();
-        });
-        
-        $(`#manth-title`).click(function()
-        {
-          $(`#day-title`).show();
-        });
